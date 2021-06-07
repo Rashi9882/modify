62f4f18b68d0f1f49e6a1c0d6e0c341e5b3575aa
unknown <guptarashi578@gmail.com>
2021-06-07 21:53:47 +0530

    error

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import LoginCard from './LoginCard';
-import {Navbar,NavbarBrand} from 'reactstrap';
 
 export class Login extends React.Component {
      render() {
          return (
             <div className="App">
-            {/* <Navbar dark color="info">
-                <div className="container">
-                    <NavbarBrand href="/">
-                        HelloMusic
-                    </NavbarBrand>
-                </div>
-            </Navbar> */}
-            <LoginCard/>
+            
             </div>
          );
      }
